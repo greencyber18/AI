b804b3891d323f577133532ccffe2ad06b7a7bc0
greencyber18 <39981392+greencyber18@users.noreply.github.com>
2021-05-30 11:33:41 +0600

    commiting a py file

---
+++ listandother.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 11:28:13 2021
+
+@author: rafsh
+"""
+
---
+++ random.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 12:55:13 2021
+
+@author: rafsh
+"""
+
+print("Rafshan")
+
+
+
+i=int(input());
+j=int(input());
+
+k=i+j;
+
+print('output =' ,k)
+print(type(k))
+
+if i>j:
+    print("I is bigger")
+else:
+        print("J is bigger")
+    
+
+ 
+
